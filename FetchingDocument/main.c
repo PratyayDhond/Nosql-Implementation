21058292fa361789303feb596b55f42b760d17ca
sohel4040 <sohelbargir2@gmail.com>
2023-05-26 14:22:33 +0530

    Functions - CreateDocument, updateDocument and deleteDocument code done

--- FetchingDocument/main.c
+++ FetchingDocument/main.c
@@ -4,18 +4,39 @@
 
 int main()
 {
-    document *d= getDocument("Posts","post3");
-
-    if(d) displayDocument(*d);
-
-    strcpy(d->key,"sohel");
+    // Code for getting a single document
+    // document *d= getDocument("Posts","post3");
+    // if(d) displayDocument(*d);
 
 
-    int result = createDocument("Posts", d);
+    // Code for creating and initlizing the doucment
+    // Pair pairs;
+    // initPair(&pairs);
+    // createPair(&pairs, "name", "Sohel Bargir","string");
+    // createPair(&pairs, "subject", "DSA","string");
+    // createPair(&pairs, "College", "COEP","string");
 
+    // document* doc = initilizeAndCreateDocument("documentpath", pairs);
+    // int result = createDocument("Posts", doc);
+
+
+    // Code for getting all documents from collection
     // Collection col = getAllDocumentFromCollection("Posts");
-
     // displayCollection(col);
 
+    // Code for updating a single document
+    // Pair pairs;
+    // initPair(&pairs);
+    // createPair(&pairs, "name", "Aditya Raul","string");
+    // createPair(&pairs, "subject", "DSA","string");
+    // createPair(&pairs, "College", "COEP","string");
+
+    // document* doc = initilizeAndCreateDocument("documentpath", pairs);
+    // int status = updateDocument("Posts", doc);
+
+    // Code for deleting a single document
+    // int status = deleteDocument("Posts", "newpost");
+
+    
     return 0;
 }
