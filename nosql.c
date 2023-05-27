256c53dda2d538767950d2f12a134943798376d7
sarvesh123305 <sarvesh123305@gmail.com>
2023-05-28 00:22:55 +0530

    Manual Page updated

--- nosql.c
+++ nosql.c
@@ -1331,7 +1331,7 @@ void noSQLMenu()
 {
     initGlobals();
     ioctl(0, TIOCGWINSZ, &sz);
-    test1();
+    // test1();
     printWelcomeMessage();
     int command;
     int programRunning = 1;
