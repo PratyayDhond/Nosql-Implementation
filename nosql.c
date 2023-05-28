525797e27f79f1c1572f49e8be28f32771faf352
sarvesh123305 <sarvesh123305@gmail.com>
2023-05-28 08:55:42 +0530

    removing unneccessary files from the folder

--- nosql.c
+++ nosql.c
@@ -1347,7 +1347,7 @@ void noSQLMenu()
 {
     initGlobals();
     ioctl(0, TIOCGWINSZ, &sz);
-    test1();
+    // test1();
     printWelcomeMessage();
     int command;
     int programRunning = 1;
